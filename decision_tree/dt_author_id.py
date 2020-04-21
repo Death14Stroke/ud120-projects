1cf5489f1ab7b08c3ded718768ad64d241e7a4af
DeaDshot14 <mistry14darren@gmail.com>
2020-04-21 22:32:47 +0530

    decision tree mini project

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -9,25 +9,35 @@
     Chris has label 1
 
 """
-    
+
 import sys
 from time import time
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
+# TODO: mini-project
 #########################################################
 ### your code goes here ###
 
+# TODO: number of features
+print("number of features =", len(features_train[0]))
 
+from sklearn.tree import DecisionTreeClassifier
+
+clf = DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+
+pred = clf.predict(features_test)
+
+# TODO: calculate accuracy
+from sklearn.metrics import accuracy_score
+
+accuracy = accuracy_score(pred, labels_test)
+print("accuracy = ", round(accuracy, 3))
 #########################################################
-
-
